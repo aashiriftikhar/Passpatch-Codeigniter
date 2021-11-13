2ac28298615b4adb0f3b0e291910fe4fb283d01e
Aashir Iftikhar <aashir_iftikhar@live.com>
2021-11-14 04:11:48 +0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -27,9 +27,9 @@ $baseURL = (isset($_SERVER["HTTPS"]) ? "https://" : "http://").$_SERVER["HTTP_HO
 $baseURL .= str_replace(basename($_SERVER["SCRIPT_NAME"]), "", $_SERVER["SCRIPT_NAME"]);
 
 // $config['base_url'] 	= 'http://localhost/pass/';
-$config['base_url'] 	= 'http://'. $_SERVER['HTTP_HOST'] .'/aashir/';
-$config['public_url'] 	= 'http://'. $_SERVER['HTTP_HOST'] .'/aashir/'.'assets/';
-$config['upload_url'] 	= 'http://'. $_SERVER['HTTP_HOST'] .'/aashir/'.'uploads/';
+$config['base_url'] 	= 'http://'. $_SERVER['HTTP_HOST'];
+$config['public_url'] 	= 'http://'. $_SERVER['HTTP_HOST'].'assets/';
+$config['upload_url'] 	= 'http://'. $_SERVER['HTTP_HOST'].'uploads/';
 /*
 |--------------------------------------------------------------------------
 | Index File
